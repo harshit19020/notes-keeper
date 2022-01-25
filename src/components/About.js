fdf2c65528c983bdc7b3e57c9b03643b4ca09f9b
samkitjain1802 <jainsamkit816@gmail.com>
2022-01-25 10:41:53 +0530

    commit

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React, {useContext, useEffect} from 'react'
+
+export const About = () => {
+    // const a = useContext(noteContext)
+    return (
+        <div>
+             This is About page 
+        </div>
+    )
+}
+
+export default About
---
+++ src/components/Noteitem.js
@@ -0,0 +1,30 @@
+import React, {useContext} from 'react'
+import noteContext from '../context/notes/noteContext';
+
+const Noteitem = (props) => {
+    const context = useContext(noteContext);
+    const { note, updateNote } = props;
+    const { deleteNote } = context;
+    return (
+        <div className="col-md-3">
+            <div className="card my-3">
+                <div className="card-body">
+                    <div className="d-flex align-items-center">
+                        <h5 className="card-title">{note.title}</h5>
+                        <i className="fas fa-trash mx-2" onClick={()=>{
+                            deleteNote(note._id);
+                            props.showAlert("Deleted Successfully", "success");
+                        }}></i>
+                        <i className="fas fa-edit mx-2" onClick={()=>{
+                            updateNote(note);
+                            // props.showAlert("Updated Successfully", "success");
+                        }}></i>
+                    </div>
+                        <p className="card-text">{note.description}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Noteitem
